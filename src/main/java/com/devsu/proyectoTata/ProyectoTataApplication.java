a782f31d7bc0933df6a2a5c6d5bced3cd8c5628f
Jorge Yánez <yanezj25@hotmail.com>
2024-05-17 12:01:39 -0500

    Merge origin/proyecto into proyecto2

---
+++ src/main/java/com/devsu/proyectoTata/ProyectoTataApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.proyectoTata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTataApplication.class, args);
+	}
+
+}
